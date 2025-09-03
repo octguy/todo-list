801738691a5e5195965d9d7c34a1f963a75cda7b
OctGuy <heyiamthinh1003@gmail.com>
2025-09-03 15:15:46 +0700

    fix: expiration of jwt token (1 day instead of 1000 days)

--- todo-be/src/main/java/com/example/todobe/jwt/JwtUtil.java
+++ todo-be/src/main/java/com/example/todobe/jwt/JwtUtil.java
@@ -63,7 +63,7 @@ public class JwtUtil {
                 .setClaims(claims)
                 .setSubject(subject)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + expiration * 1000))
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
